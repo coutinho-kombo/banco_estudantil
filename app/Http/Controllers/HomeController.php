06ba9e6210833ffc4ae56a1bf86eea521cd9f8eb
Nicolau NP <mr1Normaliii@gmail.com>
2021-04-13 20:35:55 +0100

    trabalhando no layout

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,22 +7,18 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
     /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-
-    /**
      * Show the application dashboard.
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
     public function index()
     {
-        return view('home');
+        $data = [
+            'title'=>"Banco Estudantil",
+            'menu'=>"Home",
+            'submenu'=>null,
+            'type'=>"home"
+        ];
+        return view('home', $data);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,5 +2,4 @@
 
 @section('content')
 
-home
 @endsection
